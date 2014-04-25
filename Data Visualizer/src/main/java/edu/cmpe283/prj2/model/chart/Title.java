6042a59ff047ef8c19bb40dc4a69b4f728ea9596
rajkiranpro <rajkiranpro@gmail.com>
2014-04-25 11:58:23 -0700

    Title Code

---
+++ Data Visualizer/src/main/java/edu/cmpe283/prj2/model/chart/Title.java
@@ -0,0 +1,24 @@
+package edu.cmpe283.prj2.model.chart;
+
+/**
+ * User: vplociennik
+ * Date: 12/1/13
+ * Time: 1:32 AM
+ */
+public class Title {
+
+
+    private String text;
+
+    public Title(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
